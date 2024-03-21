9a3ec32dafd9a848865a246de4d026fab773107a
nif6426 <nif6426@thi.de>
2024-03-21 14:47:22 +0100

    SWEP2 initial commit

---
+++ SWEP2/steichholzspiel/src/main/java/com/streich/Main.java
@@ -0,0 +1,8 @@
+package com.streich;
+
+public class Main {
+Spiel spiel = new Spiel();
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ SWEP2/steichholzspiel/src/main/java/com/streich/Spiel.java
@@ -0,0 +1,7 @@
+package main.java.com.streich; // Update the package declaration to match the expected package
+
+public class Spiel {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
